aeb06a85729dfec0940eea6e08b5820abbf5f5d6
Adnan Ebrahimi <adnan.ebrahimi@gmail.com>
2022-12-14 11:59:28 +0330

    angularize

---
+++ src/app/pages/layouts/sidenav-layout/sidenav-layout.component.ts
@@ -0,0 +1,25 @@
+import { Component } from '@angular/core';
+import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
+import { Observable } from 'rxjs';
+import { map, shareReplay } from 'rxjs/operators';
+import {MatSidenavModule} from '@angular/material/sidenav';
+
+
+@Component({
+  selector: 'app-sidenav-layout',
+  templateUrl: './sidenav-layout.component.html',
+  styleUrls: ['./sidenav-layout.component.css'],
+  imports: [MatSidenavModule],
+  standalone: true
+})
+export class SidenavLayoutComponent {
+
+  isHandset$: Observable<boolean> = this.breakpointObserver.observe(Breakpoints.Handset)
+    .pipe(
+      map(result => result.matches),
+      shareReplay()
+    );
+
+  constructor(private breakpointObserver: BreakpointObserver) {}
+
+}
---
+++ src/app/pages/main/main.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './main.component.html',
+  styleUrls: ['./main.component.scss']
+})
+export class MainComponent {
+
+}
