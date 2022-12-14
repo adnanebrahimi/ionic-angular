9db5e831cc300f948e39a564f9955de510e1f166
Adnan Ebrahimi <adnan.ebrahimi@gmail.com>
2022-12-14 18:21:50 +0330

    added nfc

--- src/app/pages/nfc/nfc.component.ts
+++ src/app/pages/nfc/nfc.component.ts
@@ -1,12 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NFC, Ndef } from '@awesome-cordova-plugins/nfc/ngx';
+import { Observable } from 'rxjs';
 
 @Component({
   standalone: true,
   imports: [CommonModule],
   templateUrl: './nfc.component.html',
-  styleUrls: ['./nfc.component.scss']
+  styleUrls: ['./nfc.component.scss'],
 })
-export class NFCComponent {
-
+export class NFCComponent implements OnInit {
+  public readerMode$!: Observable<any>;
+  constructor(private nfc: NFC, private ndef: Ndef) {}
+  ngOnInit(): void {
+    // Read NFC Tag - Android
+    // Once the reader mode is enabled, any tags that are scanned are sent to the subscriber
+    let flags = this.nfc.FLAG_READER_NFC_A | this.nfc.FLAG_READER_NFC_V;
+    this.readerMode$ = this.nfc.readerMode(flags);
+  }
 }
